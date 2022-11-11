1241f5f7913993f5e16239a125177ba8549a5838
Lee-na-yun <happyin3333@nate.com>
2022-11-11 22:26:27 +0900

    글목록페이지 디자인

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledItemBoxDiv = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  height:100px;
+  margin:20px;
+`;
 
 const ListPage = () => {
+  // 최초 데이터
+  const [posts, setPosts] = useState([
+    { id: 1, title: "내용1" },
+    { id: 2, title: "내용2" },
+    { id: 3, title: "내용3" },
+    { id: 4, title: "내용4" },
+    { id: 5, title: "내용5" },
+  ]);
+
   return (
     <div>
       <h1>글목록 페이지</h1>
+      <hr />
+      {posts.map((posts) => (
+        <StyledItemBoxDiv>
+          번호 : {posts.id} 제목 : {posts.title}
+        </StyledItemBoxDiv>
+      ))}
     </div>
   );
 };
